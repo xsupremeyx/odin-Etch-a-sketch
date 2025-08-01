0bb05e15c2253af6b279efb111b3045d91674334
Yash <86472828+xsupremeyx@users.noreply.github.com>
2025-08-01 16:48:26 +0000

    Add hover effect to grid items

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+function gridMaker(){
+    for(let i = 0; i < 16*16; i++ ){
+        const tile = document.createElement("div");
+        tile.classList.add("tile");
+        container.appendChild(tile);
+    }
+}
+
+function hoverMouse(tileset){
+    tileset.forEach(tile => {
+        tile.addEventListener("mouseover", (event) => {
+            event.target.classList.add("hovered");
+        });
+    });
+}
+
+
 const container = document.querySelector("#container");
 container.classList.add("container")
+gridMaker();
+const tileset = document.querySelectorAll(".tile");
+hoverMouse(tileset);
 
 
 
-for(let i = 0; i < 16*16; i++ ){
-    const tile = document.createElement("div");
-    tile.classList.add("tile");
-    container.appendChild(tile);
-}
+
