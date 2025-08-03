f422155d5f36abe9b7102b33279d0477e5f2e8fd
Yash <86472828+xsupremeyx@users.noreply.github.com>
2025-08-03 09:40:10 +0000

    Major progress done overall on project
    additions done:
    1. grid resize button
    2. grid making
    
    Known issues:
    uneven container size with respect to number of
    tiles eg. 35, 100 tile sizes have uneven spacing

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function gridMaker(){
-    for(let i = 0; i < 16*16; i++ ){
+function gridMaker(container,num){
+    
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    const containerSize = 960;
+    const borderSize = 2;
+    const tSize =+(containerSize / num).toFixed(2);
+
+    for(let i = 0; i < num*num; i++ ){
         const tile = document.createElement("div");
         tile.classList.add("tile");
         container.appendChild(tile);
+        tile.style.width = `${tSize - borderSize}px`;
+        tile.style.height = `${tSize - borderSize}px`;
     }
 }
 
@@ -14,16 +25,41 @@ function hoverMouse(tileset){
     });
 }
 
-function buttonClick(){
+function promptBttn(input){
+
+    let numtiles = prompt("Enter the number of tileset: ",input.value);
+    return +numtiles;
+}
+
+function buttonClick(container){
     //WORK OVER HERE
+    const setTilebttn = document.querySelector("#resize");
+
+    setTilebttn.addEventListener("click",()=>{
+        const input = document.querySelector("#gridSize");
+        let numtiles = promptBttn(input);
+        if (isNaN(numtiles) || numtiles <= 0 || numtiles > 100) {
+            numtiles = 16;
+            input.value = 16;
+        }
+        input.setAttribute("value",numtiles);
+        gridMaker(container,numtiles);
+        const tileset = document.querySelectorAll(".tile");
+        hoverMouse(tileset);
+
+    });
 }
 
 
 const container = document.querySelector("#container");
 container.classList.add("container")
-gridMaker();
+gridMaker(container,16);
 const tileset = document.querySelectorAll(".tile");
 hoverMouse(tileset);
+buttonClick(container);
+
+
+
 
 
 
